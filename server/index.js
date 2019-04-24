2d1c980b5d404b4f6631264a9b92e3152e3644a7
Niklas Appelroth <niklas.appelroth@barona.fi>
2019-04-24 15:20:47 +0300

    Extract schema

--- server/index.js
+++ server/index.js
@@ -1,50 +1,8 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server')
 const jwt = require('jsonwebtoken')
 const userDb = require('./db/users')
 const tweetDb = require('./db/tweets')
-
-const typeDefs = gql`
-  type Query {
-    user(username: String!): User
-    users: [User]!
-    tweet(id: ID!): Tweet
-    tweets: [Tweet]!
-    me: User
-  }
-  type Mutation {
-    createUser(
-      username: String!,
-      displayName: String,
-      bio: String,
-      photo: String
-    ): User
-    updateUser(
-      username: String!,
-      displayName: String,
-      bio: String,
-      photo: String
-    ): User
-    deleteUser(id: ID!): User
-    createTweet(tweet: String!, from: String!): Tweet
-    deleteTweet(id: ID!): Tweet
-  }
-  type User {
-    id: ID!
-    createdAt: String!
-    username: String!
-    displayName: String
-    bio: String
-    email: String
-    photo: String
-    tweets: [Tweet]
-  }
-  type Tweet {
-    id: ID!
-    createdAt: String!
-    tweet: String!
-    from: User!
-  }
-`
+const typeDefs = require('./schema')
 
 // (obj, args, context, info) => result
 const resolvers = {
---
+++ server/schema.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server')
+
+module.exports = gql`
+  type Query {
+    user(username: String!): User
+    users: [User]!
+    tweet(id: ID!): Tweet
+    tweets: [Tweet]!
+    me: User
+  }
+
+  type Mutation {
+    createUser(
+      username: String!,
+      displayName: String,
+      bio: String,
+      photo: String
+    ): User
+    updateUser(
+      username: String!,
+      displayName: String,
+      bio: String,
+      photo: String
+    ): User
+    deleteUser(id: ID!): User
+    createTweet(tweet: String!, from: String!): Tweet
+    deleteTweet(id: ID!): Tweet
+  }
+
+  type User {
+    id: ID!
+    createdAt: String!
+    username: String!
+    displayName: String
+    bio: String
+    email: String
+    photo: String
+    tweets: [Tweet]
+  }
+
+  type Tweet {
+    id: ID!
+    createdAt: String!
+    tweet: String!
+    from: User!
+  }
+`
