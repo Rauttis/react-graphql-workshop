31996c0389c31141b264c56d4a004a80e8317807
Niklas Appelroth <niklas.appelroth@barona.fi>
2019-04-24 16:29:25 +0300

    Fix navbar photo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,6 +14,7 @@ const currentUserQuery = gql`
       displayName
       email
       bio
+      photo
     }
   }
 `
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -75,7 +75,6 @@ const Home = ({ loading, me }) => {
           />
         }}
       </Query>
-
     </Container>
   );
 };
