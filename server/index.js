851be659123e651b26832142d14b3d0e643e2b36
Niklas Appelroth <niklas.appelroth@barona.fi>
2019-04-24 09:50:49 +0300

    Exercise 2

--- server/index.js
+++ server/index.js
@@ -1,1 +1,45 @@
-console.log('Hello world!');
+const { ApolloServer, gql } = require('apollo-server')
+const userDb = require('./db/users')
+const tweetDb = require('./db/tweets')
+
+const typeDefs = gql`
+  type Query {
+    user(username: String!): User
+    users: [User]!
+    tweet(id: ID!): Tweet
+    tweets: [Tweet]!
+  }
+  type User {
+    id: ID!
+    createdAt: String!
+    username: String!
+    displayName: String
+    bio: String
+    email: String
+    photo: String
+    tweets: [Tweet]
+  }
+  type Tweet {
+    id: ID!
+    createdAt: String!
+    tweet: String!
+    from: User!
+  }
+`
+
+// (obj, args, context, info) => result
+const resolvers = {
+  Query: {
+    user: (_, {username}) => userDb.getUserByUsername(username),
+    users: () => userDb.getAllUsers(),
+    tweet: (_, {id}) => tweetDb.getTweetById(id),
+    tweets: () => tweetDb.getAllTweets(),
+  },
+  User: {
+    tweets: ({username}) => tweetDb.getTweetsFrom(username),
+  }
+}
+
+const server = new ApolloServer({ resolvers, typeDefs })
+
+server.listen().then(server => console.log(`Server started at ${server.url}`))
